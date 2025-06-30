ac2afbdb3fca04fc59b2bc24888b635377c274e0
Natnicha <nat.rodtong@gmail.com>
2025-06-30 15:08:12 +0200

    feat: add radix theme

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { Theme, ThemePanel  } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Theme accentColor="crimson" grayColor="sand" radius="large" scaling="95%">
+      <App />
+      <ThemePanel />
+    </Theme>
   </StrictMode>,
 )
