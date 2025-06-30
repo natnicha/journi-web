dd92f0037267ade702a1136b39e13bdfc2fa57f6
Natnicha <nat.rodtong@gmail.com>
2025-06-30 19:44:58 +0200

    refactor: remake nav bar

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Flex, Container, TabNav, Theme} from "@radix-ui/themes";
+import { Flex, Container, Tabs, Theme} from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import { Menu } from "lucide-react";
 import App from './App.tsx'
@@ -15,11 +15,23 @@ createRoot(document.getElementById('root')!).render(
         </div>
 
         <Flex className="nav-links" direction="column" gap="2">
-          <TabNav.Root justify={'center'} size="2">
-            <TabNav.Link href="#">Home</TabNav.Link>
-            <TabNav.Link href="#" active>Trips</TabNav.Link>
-            <TabNav.Link href="#">Destinations</TabNav.Link>
-          </TabNav.Root>
+          <Tabs.Root defaultValue="tab2" >
+            <Tabs.List style={{ display: 'flex', gap: 12 }} justify={"center"}>
+              <Tabs.Trigger style={{ fontSize: '18px', padding: '8px 16px', cursor: 'pointer' }} value="tab1" >
+                Home
+              </Tabs.Trigger>
+              <Tabs.Trigger style={{ fontSize: '18px', padding: '8px 16px', cursor: 'pointer' }} value="tab2" >
+                Trips
+              </Tabs.Trigger>
+              <Tabs.Trigger style={{ fontSize: '18px', padding: '8px 16px', cursor: 'pointer' }} value="tab3" >
+                Destinations
+              </Tabs.Trigger>
+            </Tabs.List>
+
+            <Tabs.Content value="tab1"></Tabs.Content>
+            <Tabs.Content value="tab2"></Tabs.Content>
+            <Tabs.Content value="tab3"></Tabs.Content>
+          </Tabs.Root>
         </Flex>
 
         <div className="account">
