025b7d722e806e66e81dcda3cc4685c7a2b9e1ef
Natnicha <nat.rodtong@gmail.com>
2025-07-09 22:36:54 +0200

    feat: adjust marker color on map and cost input bg color according to days

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -19,6 +19,7 @@ import './LeafletNumberedMarkers.css'
 const zoom_default: number = 15
 
 type PlaceInfo = {
+  day: number;
   id: number;
   order?: number;
   position: LatLngExpression;
@@ -51,6 +52,7 @@ function ReverseGeocodeMarker({ addNewPlace }: { addNewPlace: (location: PlaceIn
       try {
         const plc: LatLngExpression = [lat, lng];
         var place = {
+          day: 1,
           id: 0,
           position: plc,
           title: "",
@@ -75,27 +77,43 @@ function App() {
   const plc5: LatLngExpression = [13.725474885078526, 100.5338972116344]; //Restaurant
   const plc6: LatLngExpression = [13.733148327431199, 100.5271507885528]; //Hotel
   const plc7: LatLngExpression = [13.735912709116183, 100.52462681828554]; //Cafe
+  const plc8: LatLngExpression = [13.741696859976935, 100.4963759526378]; //Pak Khlong Talat (Flower Market)
+  const plc9: LatLngExpression = [13.737331527286672, 100.49547754359172]; //Wat Prayurawongsawas Worawihan
+  const plc10: LatLngExpression = [13.736647292410602, 100.49946429489435]; //Princess Mother Memorial Park
+  const plc11: LatLngExpression = [13.737392014744712, 100.50011779614503]; //Guan Yu Shrine (Khlong San)
 
   const markers: PlaceInfo[] = [
-    { id: 1, order:1, position: plc1, title:"Wat Ratchanatdaram Worawihan", detail:"วัด​ราชนัดดารามวรวิหาร​",
+    { day: 1, id: 1, order:1, position: plc1, title:"Wat Ratchanatdaram Worawihan", detail:"วัด​ราชนัดดารามวรวิหาร​",
       src: "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nocKS_n7LfYXrjscSLL_inNyakjdm3YMtFV0NZgHGQA0R2akgrcz3TrI-RrJxVAgVmdg7yAN8ywEZmYZK8UEff7q9cm4tUCWHoqp57KGA5c7Xsq8NnL3yOiLAe02Uz3GXDen0U=w408-h544-k-no"
     },
-    { id: 2, order:2, position: plc2, title:"Sao Chingcha", detail:"เสาชิงช้า",
+    { day: 1, id: 2, order:2, position: plc2, title:"Sao Chingcha", detail:"เสาชิงช้า",
       src: "https://lh3.googleusercontent.com/gps-cs-s/AC9h4np6sSA4saMvYxbBlbZ0yvJC44Da6hz8DTkc44iGX8i8n1_XLQ_XVISvbJGLi_ayhY8k0gIX26qIl9M-__wTX_f5fohMH5DngaiQL9XTnWnIdwdi380JwPWJQ5nJ180F-mwoPvBxAw=w408-h725-k-no"
     },
-    { id: 3, order:3, position: plc3, title:"The Grand Palace", detail:"พระบรมมหาราชวัง",
+    { day: 1, id: 3, order:3, position: plc3, title:"The Grand Palace", detail:"พระบรมมหาราชวัง",
       src: "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nojNwN-9BoahfZK7mGGJh2pazTiENSQH06r2MiOlIfcmHOjCRU2CePZ-uIeUcT1CX1Jo9WDBzOdlFrMXakp1w82BRCPOHCh0hbCoYZyzOEX5VdVD_k5op2vTc29gahLzcM2bcRcjA=w408-h272-k-no"
     },
-    { id: 4, order:4, position: plc4, title:"China Town", detail:"ไชน่าทาวน์ (เยาวราช)",
+    { day: 1, id: 4, order:4, position: plc4, title:"China Town", detail:"ไชน่าทาวน์ (เยาวราช)",
       src: "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nr3EvywM6eKq3FLgi19LxJ6PX8nR5BRAStTOHsJZszJzZ7dyIxkyRKVRjxwEl7cy5Ts4cYKy_ollUo1O2E1_nw3lr-s2QYh4diQ0PBS9Bt70g9sukwkKg4XJ-RY-O1K8kXwXFW5=w408-h306-k-no"
     },
-    { id: 5, order:5, position: plc5, title:"EAT ME RESTAURANT", detail:"อีทมี เรสเตอรองท์",
+    { day: 1, id: 5, order:5, position: plc5, title:"EAT ME RESTAURANT", detail:"อีทมี เรสเตอรองท์",
       src: "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nr0wwWWc8ZaFMwX6ol5XZmGjeS179fZldbEsY-G18DwHQB5bCG_b-sdlk41HViEk9ZdTu_HHQkxYjURMCROPBNp8gKPyTpusxmaqdfVGyhACAMR99967uuMq9RDztMt9gvHk3XRaw=w408-h306-k-no"
     },
-    { id: 6, order:6, position: plc6, title:"Mandarin Hotel Bangkok, managed by Centre Point", detail:"โรงแรมแมนดาริน กรุงเทพฯ",
+    { day: 1, id: 6, order:6, position: plc6, title:"Mandarin Hotel Bangkok, managed by Centre Point", detail:"โรงแรมแมนดาริน กรุงเทพฯ",
       src: "https://lh3.googleusercontent.com/p/AF1QipO47M8DCVLRu67siHMq6UHgX7DixyMI_iAIr4fE=w408-h279-k-no"
     },
-    { id: 7, order:7, position: plc7, title:"White Flower Cafe", detail:"ครัวดอกไม้ขาว",
+    { day: 1, id: 7, order:7, position: plc7, title:"White Flower Cafe", detail:"ครัวดอกไม้ขาว",
+      src: "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nq5c2znLp8Y7yfXYB-lTB2x2KRnXFcBKV8nmbEEDfPOQsYWYskOE6aWe287ZyI8T-YShGihssVCc_7BqzHto-eSPyfhox2WacVs1AhzNAyc1Rl5_brDKPw-_mEh7a6a60qgMXTa=w408-h544-k-no"
+    },
+    { day: 2, id: 8, order:1, position: plc8, title:"Pak Khlong Talat (Flower Market)", detail:"ปากคลองตลาด",
+      src: "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nq5c2znLp8Y7yfXYB-lTB2x2KRnXFcBKV8nmbEEDfPOQsYWYskOE6aWe287ZyI8T-YShGihssVCc_7BqzHto-eSPyfhox2WacVs1AhzNAyc1Rl5_brDKPw-_mEh7a6a60qgMXTa=w408-h544-k-no"
+    },
+    { day: 3, id: 9, order:1, position: plc9, title:"Wat Prayurawongsawas Worawihan", detail:"วัดประยุรวงศาวาสวรวิหาร",
+      src: "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nq5c2znLp8Y7yfXYB-lTB2x2KRnXFcBKV8nmbEEDfPOQsYWYskOE6aWe287ZyI8T-YShGihssVCc_7BqzHto-eSPyfhox2WacVs1AhzNAyc1Rl5_brDKPw-_mEh7a6a60qgMXTa=w408-h544-k-no"
+    },
+    { day: 4, id: 10, order:1, position: plc10, title:"Princess Mother Memorial Park", detail:"อุทยานเฉลิมพระเกียรติสมเด็จพระศรีนครินทราบรมราชชนนี",
+      src: "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nq5c2znLp8Y7yfXYB-lTB2x2KRnXFcBKV8nmbEEDfPOQsYWYskOE6aWe287ZyI8T-YShGihssVCc_7BqzHto-eSPyfhox2WacVs1AhzNAyc1Rl5_brDKPw-_mEh7a6a60qgMXTa=w408-h544-k-no"
+    },
+    { day: 5, id: 11, order:1, position: plc11, title:"Guan Yu Shrine (Khlong San)", detail:"ศาลเจ้ากวนอู (คลองสาน)",
       src: "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nq5c2znLp8Y7yfXYB-lTB2x2KRnXFcBKV8nmbEEDfPOQsYWYskOE6aWe287ZyI8T-YShGihssVCc_7BqzHto-eSPyfhox2WacVs1AhzNAyc1Rl5_brDKPw-_mEh7a6a60qgMXTa=w408-h544-k-no"
     },
   ];
@@ -205,6 +223,21 @@ function App() {
     }
     return 'Pick a date range';
   };
+
+  const getCostColorBG = (day: number) => {
+    if (day%4 == 0) return 'var(--color-tropical-sunset-sorbet-Mist)';
+    else if (day%4 == 1) return 'var(--color-tropical-palm-misty-palm)';
+    else if (day%4 == 2) return 'var(--color-tropical-lagoon-whisper)';
+    else return 'var(--color-tropical-mango-tin)';
+  };
+
+  const getMarkerSrc = (day: number) => {
+    if (day%4 == 0) return "./src/assets/map-pin-rose-sunset.png";
+    else if (day%4 == 1) return "./src/assets/map-pin.png";
+    else if (day%4 == 2) return "./src/assets/map-pin-lagoon-blue.png";
+    else return "./src/assets/map-pin-mango-deep.png";
+  };
+  
   return (
     <>
       <Grid columns="2" gap="0" rows="1" width="auto">
@@ -214,8 +247,8 @@ function App() {
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <ReverseGeocodeMarker addNewPlace={addNewPlace} />
-            {items.map(({ order, position, title, detail }) => (
-              <Marker key={order} position={position} icon={new NumberedDivIcon({number:order} as any)}>
+            {items.map(({ day, order, position, title, detail }) => (
+              <Marker key={day+","+order} position={position} icon={new NumberedDivIcon({number:order, iconUrl:getMarkerSrc(day)} as any)}>
                 <Popup> 
                   {title} <br/> {detail} <br/>
                 </Popup> 
@@ -261,7 +294,7 @@ function App() {
                       <img className='bin' src={"./src/assets/bin.png"}/>
                     </div>
                     <div>
-                      <img src={"./src/assets/map-pin.png"}/>
+                      <img src={getMarkerSrc(item.day)} />
                       <div className="pin-on-card">
                         {item.order}
                       </div>
@@ -315,9 +348,9 @@ function App() {
                     </Box>
                     )}
                   </Flex>
-                    <input className="cost"  pattern='^-?(?:0|[1-9]\d{0,2}(?:,?\d{3})*)(?:\.\d+)?$'  placeholder="$" value={item.cost} onChange={(e) =>
+                    <input className="cost" style={{ backgroundColor: getCostColorBG(item.day) }}  pattern='^-?(?:0|[1-9]\d{0,2}(?:,?\d{3})*)(?:\.\d+)?$'  placeholder="$" value={item.cost} onChange={(e) =>
                 handleChange(item.id, items as PlaceInfo[], e.target.value)} onBlur={() => handleBlur(item.id, items as PlaceInfo[])}/>
-                  </Flex>
+                    </Flex>
                   <SortableList.DragHandle />
                 </SortableList.Item>
               )}
